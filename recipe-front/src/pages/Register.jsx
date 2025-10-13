7bb6d1ca1a1a3eeb1379c5edc7dbaaa394899ad5
EdoAbarca <eduardo.abarca.c@usach.cl>
2025-10-13 17:10:29 -0300

    [MFLP-59] [MFLP-60] [MFLP-61] [MFLP-62] tickets covered
    Frontend React app initialized with relevant dependencies and instructions described in the .md file. Also, an already tested dev Dockerfile script was added for isolation and further run.

---
+++ recipe-front/src/pages/Register.jsx
@@ -0,0 +1,9 @@
+function Register() {
+  return (
+    <>
+    <h1>Welcome to the Register Page</h1>
+    <p>Here you will use your credentials to register an account.</p>
+    </>
+  );
+}
+export default Register;
